ba47f7365112242196dbd6ebd49c111a7a840233
Shems <96719853+IsShems@users.noreply.github.com>
2023-08-30 10:57:59 +0400

    Add files via upload

---
+++ Converter/Program.cs
@@ -0,0 +1,34 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
+
+/*Сделайте свой конвертер валют как valyuta.az
+
+Можно использовать только этот api
+
+https://www.cbar.az/currencies/17.07.2023.xml
+
+Удачи ))) 
+*/
