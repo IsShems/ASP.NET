391e365dbb576994bf867d76ecd6f23f4eea0b83
Shems <96719853+IsShems@users.noreply.github.com>
2023-09-09 00:03:39 +0400

    Update Quote.cs

--- ASP.NET_Core/Model/Quote.cs
+++ ASP.NET_Core/Model/Quote.cs
@@ -1,12 +1,17 @@
-﻿namespace ASP.NET_Core_.Model
+namespace ASP.NET_Core_.Model
 {
+
+
+    public class Result : Quote
+    {
+        public List<Quote> quotes { get; set; }
+    }
+
     public class Quote
     {
-        public string? Text { get; set; }
-        public string? Author { get; set; }
-
-        public Quote()
-        {
-        }
+        public int id { get; set; }
+        public string quote { get; set; }
+        public string author { get; set; }
     }
+
 }
